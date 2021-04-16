2f456e138cfc3981dd39f5a3d4ccdbc7d87eab08
rolfrander <rolfn-github@pvv.org>
2021-04-16 20:56:07 +0200

    timeout 0

--- Scripts/Install-XMouse.ps1
+++ Scripts/Install-XMouse.ps1
@@ -15,3 +15,10 @@ $systemParamInfo::SystemParametersInfo(0x1001, 0, [REF]$newVal, 2)
 # det er også noe magi knyttet til
 # ((Get-ItemProperty 'HKCU:\Control Panel\Desktop' UserPreferencesMask).UserPreferencesMask |% { '{0:X2}' -f $_ }) -join ' '
 # men jeg finner ikke noe dokumetasjon av UserPreferencesMask for windows-10, finnes bare for win2k
+
+# Regkey HKCU\Control Panel\Desktop\ActiveWndTrackTimeout to something higher than 0 to Setup delay unless other window gets active
+
+# uklart hvilken av disse som virker, ser ut til at windows må restarte for effekt, restart av explorer er ikke nok
+# https://stackoverflow.com/questions/33185219/windows-10-activate-window-by-mouse-hover-delay
+Set-ItemProperty 'HKCU:\Control Panel\Desktop\' ActiveWndTrkTimeout 0
+Set-ItemProperty 'HKCU:\Control Panel\Desktop\' ActiveWndTrackTimeout 0
